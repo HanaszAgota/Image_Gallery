e8eba0c2f5c12b96d67b96aafec870a7836a8743
Agota Hanasz <agota.hanasz.1990@icloud.com>
2023-06-12 11:03:26 +0200

    webpack skeleton

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,12 +2,19 @@ const path = require('path');
 
 const SRC_DIR = path.join(__dirname, 'src');
 const OUT_DIR = path.join(__dirname, 'out');
+const PUB_DIR = path.join(__dirname, 'public');
 
 module.exports = {
-    mode: 'main',
-    entry: path.join(SRC_DIR, './src/index.js'),
+    mode: 'development',
+    entry: path.join(SRC_DIR, './index.js'),
     output: {
         path: OUT_DIR,
         filename: 'dist.js',
-    }
+    },
+    devServer: {
+        static: {
+            directory: PUB_DIR
+        }
+    },
+    devtool: 'eval-cheap-source-map'
 }
